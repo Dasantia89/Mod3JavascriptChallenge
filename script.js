3802985696193f1045b42750c2b6a013775b15a7
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-09-21 00:11:30 -0400

    created arrays for all possible characters

--- script.js
+++ script.js
@@ -67,6 +67,14 @@ function generatePassword() {
   password.getCase();
   password.getChar();
   
+    // create arrays containing all possible characters
+    var lower = [ "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var upper = [];
+    for(var i=0; i<lower.length; i++){
+      upper[i] = lower[i].toUpperCase();
+    }
+    var numeric = [1,2,3,4,5,6,7,8,9,0];
+    var special = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
 }
 
 
